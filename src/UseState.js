cada11e8306434901fb83ad4d8a843692e101bfa
Andres Pulgarin <andres.pulgarin.nopia@gmail.com>
2024-04-11 08:33:48 -0500

    definición estados independientes

--- src/UseState.js
+++ src/UseState.js
@@ -1,14 +1,22 @@
 import React from 'react'
 
+const SECURITY_CODE = "paradigma"
+
 function UseState ({ name }) {
+    const [value, setValue] = React.useState("")
     const [error, setError] = React.useState(false)
     const [loading, setLoading] = React.useState(false)
 
     React.useEffect (() => {
         setTimeout(() => {
-            setLoading(false)
-            setError(true)
-        }, 5000)
+
+            if ( value !== SECURITY_CODE) {
+                setError(true)
+            }
+
+            setLoading(false) 
+            
+        }, 3000)
     }, [loading])
     return (
         <div>
@@ -23,9 +31,18 @@ function UseState ({ name }) {
                 <p>Cargando... </p>
             )}
 
-            <input placeholder='Código de seguridad' />
+            <input 
+                placeholder='Código de seguridad' 
+                value={value}
+                onChange={(event) => {
+                    setValue(event.target.value)
+                }}
+            />
             <button
-                onClick={() => setLoading(true)}
+                onClick={() => {
+                    setError(false)
+                    setLoading(true)
+                }}
             >
                 Comprobar
             </button>
